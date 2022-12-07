e9b128cb9c11aa1394a5061552b50af6aa9c3ad2
YU000jp <111847207+YU000jp@users.noreply.github.com>
2022-12-07 23:58:26 +0900

    設定項目
    設定項目とその対応
    デザインの変更
    
    TODO

--- src/setting.js
+++ src/setting.js
@@ -1,12 +1,57 @@
 export const settingUI = () => {
+    /* https://logseq.github.io/plugins/types/SettingSchemaDesc.html */
     const settingsTemplate = [
         {
-            key: "setting001",
-            type: "string",
-            default: `true`,
-            title: "Customize setting001",
-            description:
-                'temporary. settings are not reflected yet.',
+            key: "heading00",
+            title: "*Please reboot Logseq to reflect styles.",
+            type: "heading",
+            default: "",
+            description: "",
+        },
+        {
+            key: "switchContext",
+            title: "Add functionality context menu *",
+            type: "enum",
+            enumChoices: ["enable", "disable"],
+            enumPicker: "radio",
+            default: "enable",
+            description: "`DONE with completed`,`Link as reference`,`LATER as reference`,`Delete this block`",
+        },
+        {
+            key: "switchRightSidebar",
+            title: "Side by side in right sidebar  *",
+            type: "enum",
+            enumChoices: ["original", "normal"],
+            enumPicker: "radio",
+            default: "original",
+            description: "enable: Pages can be placed side by side in the sidebar",
+        },
+        {
+            key: "switchTagsHierarchy",
+            title: "Page tags and Hierarchy  *",
+            type: "enum",
+            enumChoices: ["Side","Bottom", "normal"],
+            enumPicker: "radio",
+            default: "Side",
+            description: "",
+        },
+        {
+            key: "switchLinkedReferences",
+            title: "Linked References Side by side  *",
+            type: "enum",
+            enumChoices: ["Side"], //normal
+            enumPicker: "radio",
+            default: "Side",
+            description: "",
+        },
+        {
+            key: "switchJournalQueries",
+            title: "Journal Queries Side by side  *",
+            type: "enum",
+            enumChoices: ["Side"], //normal
+            enumPicker: "radio",
+            default: "Side",
+            description: "",
         }
     ];
     logseq.useSettingsSchema(settingsTemplate);
