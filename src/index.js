106c8975722a6edb7461348ff78e8a9a440e6858
YU000jp <111847207+YU000jp@users.noreply.github.com>
2023-01-02 19:16:38 +0900

    processing priority

--- src/index.js
+++ src/index.js
@@ -7,20 +7,27 @@ import { ColumnLayoutStyle } from './layout';
 import { TurnOnFunction } from './function';
 import { MarkdownLink } from './markdown-link';
 
+
 /* main */
 async function main() {
     console.info(`#${pluginId}: MAIN`); /* -plugin-id */
-    settingUI();
     ColumnLayoutStyle();
+};/* end_main */
+
+
+async function after() {
+    console.info(`#${pluginId}: AFTER`); /* -plugin-id */
+    
+    //lazy loading
     setTimeout(function () {
         TurnOnFunction();
         if (logseq.settings.switchMarkdownLink === "enable") {
             MarkdownLink();
         }
-    }, 1000);
-    
+        settingUI();
+    }, 3000);
 
     console.info(`#${pluginId}: loaded`);
-};/* end_main */
+}
 
-logseq.ready(main).catch(console.error);
+logseq.ready(main).then(after).catch(console.error);
