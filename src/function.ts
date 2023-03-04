255a1e031a8b3bf47541454d8b57434dca592947
YU000jp <111847207+YU000jp@users.noreply.github.com>
2023-03-04 22:41:27 +0900

    add Test Function
    Year list Calendar

--- src/function.ts
+++ src/function.ts
@@ -112,4 +112,75 @@ export const TurnOnFunction = async (UserSettings) => {
         }
     });
 
+
+    /* SlashCommand  */
+    logseq.Editor.registerBlockContextMenuItem('Create year calendar', async (e) => {
+        const userConfigs = await logseq.App.getUserConfigs();
+        const ThisDate: any = new Date();
+        const ThisYear = ThisDate.getFullYear();
+        const ThisMonth = ThisDate.getMonth() + 1;
+        const displayThisMonth = String(ThisMonth).padStart(2, '0');
+        //logseq-dateutilsでは月Dataからは取得できない
+        logseq.Editor.insertBlock(e.uuid, `Year calendar since ${ThisYear}/${displayThisMonth}`).then(async (b) => {
+            if (b) {
+                const thisBlock = b.uuid;
+
+                //年間タスクリスト作成
+
+                // 1月から12月までの各月の日数を配列に格納
+                const monthDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+                async function createCalendar(year) {
+                    let forTry;
+                    let forNextYear;
+                    if (year === ThisYear) {
+                        forTry = 12;
+                        forNextYear = ThisMonth - 1;
+                    } else {
+                        forTry = ThisMonth;
+                        forNextYear = 0;
+                    }
+                    for (let month: number = forNextYear; month < forTry; month++) {
+                        // 月の日数を取得する
+                        let daysInMonth = monthDays[month];
+                        // 2月の場合は閏年かどうかを確認して日数を修正
+                        if (month === 1 && isLeapYear(year)) {
+                            daysInMonth = 29;
+                        }
+                        // 月のカレンダー
+                        const displayMonth = String(month + 1).padStart(2, '0');
+                        //if 日付リンク有効/無効(ユーザー形式の月表示は難しい)      #TODO
+                        await logseq.Editor.insertBlock(thisBlock, `[[${year}/${displayMonth}]]`, { properties: { collapsed: true } }).then(async (m) => {
+                            if (m) {
+                                for (var day = 1; day <= daysInMonth; day++) {
+                                    // 曜日を取得
+                                    const dayOfWeek = new Date(year, month, day).getDay();
+                                    // 平日と休日の色分け
+                                    let dayClass;
+                                    if (dayOfWeek === 0) {
+                                        dayClass = "red";
+                                    } else if (dayOfWeek === 6) {
+                                        dayClass = "blue";
+                                    } else {
+                                        dayClass = "";
+                                    }
+                                    logseq.Editor.insertBlock(m.uuid, await getDateForPage(new Date(`${year}/${displayMonth}/${day}`), userConfigs.preferredDateFormat), { properties: { "background-color": dayClass } });
+                                }
+                            }
+                        });
+                    }
+                }
+                await createCalendar(ThisYear);
+                await createCalendar(ThisYear as number + 1);
+            }
+        }).finally(() => {
+            logseq.UI.showMsg(`Year Calendar since ${ThisYear}/${displayThisMonth}`, 'info');
+        });
+    });
+
 };
+
+// うるう年かどうかを判定する関数
+function isLeapYear(year) {
+    return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
+}
