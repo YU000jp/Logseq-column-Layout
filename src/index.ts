45f45e6a0d818de862d87c5ff78c98a547712ecc
YU000jp <111847207+YU000jp@users.noreply.github.com>
2023-06-15 12:28:48 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -113,16 +113,12 @@ body[data-page="page"] div.asset-container img {
 }
 
 const removeProvideStyle = (className: string) => {
-    const doc = parent.document.head.querySelector(`style[data-injected-style^="${className}"]`);
-    if (doc) {
-        doc.remove();
-    }
+    const doc = parent.document.head.querySelector(`style[data-injected-style^="${className}"]`) as HTMLStyleElement;
+    if (doc) doc.remove();
 };
 
 function removeCSSclass(className: string): void {
-    if (parent.document.body.classList?.contains(className)) { // classNameが存在するかを確認する
-        parent.document.body.classList.remove(className);
-    }
+    if (parent.document.body.classList?.contains(className)) parent.document.body.classList.remove(className);
 }
 
 //https://logseq.github.io/plugins/types/SettingSchemaDesc.html
