af5e801f5ba62f3dd88eb3bf33f1a1f9ceb0f303
YU <111847207+YU000jp@users.noreply.github.com>
2023-08-10 17:22:40 +0900

    refactor module

--- src/lib.ts
+++ src/lib.ts
@@ -1,3 +1,4 @@
+import CSSsideLinkedReferences from "./side.css?inline";
 
 export const removeProvideStyle = (className: string) => {
     const doc = parent.document.head.querySelector(`style[data-injected-style^="${className}"]`) as HTMLStyleElement | null;
@@ -15,4 +16,23 @@ export const calculateRangeBarForSettingUI = (min: number, max: number, value: n
 };
 
 //レンジバー導入のため設定数値の互換用
-export const calculateRangeBarForSettingUIonce = (min: number, max: number, value: number): number => Math.round((value - min) / (max - min) * 100);
+export const calculateRangeBarForSettingOnce = (min: number, max: number, value: number): number => Math.round((value - min) / (max - min) * 100);
+
+export async function versionCheck(first: number, second: number, third: number): Promise<boolean> {
+    const version: string = await logseq.App.getInfo("version");
+    const versionArr = version?.split(".") as string[];
+    if (Number(versionArr[0]) > first ||
+        Number(versionArr[0]) === first &&
+        Number(versionArr[1]) > second ||
+        (Number(versionArr[1]) === second && Number(versionArr[2]) >= third)) {
+        return true; //指定した以上のバージョンの場合
+    } else return false; //指定より古いバージョンの場合
+}
+
+export function provideStyleSide(versionOver: boolean, newKey: string, newStyle: string, oldKey: string, oldStyle: string,) {
+    if (versionOver === true)
+        logseq.provideStyle({ key: newKey, style: newStyle });//指定した以上のバージョンの場合
+    else
+        logseq.provideStyle({ key: oldKey, style: oldStyle });//指定より古いバージョンの場合
+}
+
---
+++ src/settings.ts
@@ -0,0 +1,48 @@
+import { SettingSchemaDesc } from "@logseq/libs/dist/LSPlugin.user";
+import { calculateRangeBarForSettingUI } from "./lib";
+
+//https://logseq.github.io/plugins/types/SettingSchemaDesc.html
+export const settingsTemplate: SettingSchemaDesc[] = [
+    {
+        key: "booleanLinkedReferences",
+        title: "Journals, Turn on Linked References side by side",
+        type: "boolean",
+        default: true,
+        description: "default: `true`",
+    },
+    {
+        key: "booleanRightSidebar",
+        title: "Enable original right sidebar",
+        type: "boolean",
+        default: true,
+        description: "default: `true`, place blocks or pages side by side in the sidebar",
+    },
+    {
+        key: "imageSizeMaxHome",
+        title: "Change large image max-size (for journals)",
+        type: "number",
+        default: "72",
+        description: "`300` < default: `660` < `800` [px]",
+        inputAs: "range",
+    },
+    {
+        key: "imageSizeMaxPage",
+        title: "Change large image max-size (for non-journal pages)",
+        type: "number",
+        default: "83",
+        description: "`300` < default: `1050` < `1200` [px]",
+        inputAs: "range",
+    },
+];export const CSSimageSize = (home: number, page: number): string => {
+    //Home: 300px < number > 800px
+    //Page: 300px < number > 1200px
+    return `
+body[data-page="home"] div.asset-container img {
+    max-width: ${calculateRangeBarForSettingUI(300, 800, home)}px
+}
+body[data-page="page"] div.asset-container img {
+    max-width: ${calculateRangeBarForSettingUI(300, 1200, page)}px
+}
+`;
+};
+
