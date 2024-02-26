410cc49501c3c1ef9f65c0ae18b16f0ed21769c9
YU <111847207+YU000jp@users.noreply.github.com>
2024-02-26 20:28:55 +0900

    perf(): remove check Logseq version #25

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,14 @@
 import "@logseq/libs"
 import { LSPluginBaseInfo } from "@logseq/libs/dist/LSPlugin.user"
 import { setup as l10nSetup, t } from "logseq-l10n" //https://github.com/sethyuan/logseq-l10n
-import { provideStyleByVersion, provideStyleRightSidebarEachVersion, removeProvideStyle, versionCheck } from "./lib"
+import { removeProvideStyle } from "./lib"
 import CSSmain from "./main.css?inline"
-import CSS3NestingSide from "./nestingSide.css?inline"
+import CSS3Side from "./side.css?inline"
 import CSSNonSide from "./notSide.css?inline"
 import { settingsTemplate } from "./settings"
-import CSSside from "./side.css?inline"
 import ja from "./translations/ja.json"
-export let versionOver: boolean = false
-export let versionOver0914: boolean = false
+import CSS3RightSidebar from "./rightSidebar.css?inline"
 const keyRightSidebar = "rightSidebar"
-const keyNestingRightSidebar = "nestingRightSidebar"
-const keyNestingRightSidebar00914 = "nestingRightSidebar00914"
 const keySide = "side"
 const keyNestingSide = "nestingSide"
 const keyNonSide = "nonSide"
@@ -31,25 +27,15 @@ const main = async () => {
     logseq.updateSettings({ notice2023111404: true })
   }
 
-  versionOver = await versionCheck(0, 9, 11) //バージョンチェック
-  versionOver0914 = await versionCheck(0, 9, 14) //バージョンチェック
-  if (logseq.settings?.booleanLinkedReferences === true) {
-    provideStyleByVersion(
-      versionOver,
-      keyNestingSide,
-      CSS3NestingSide,
-      keySide,
-      CSSside
-    )
-  } else {
+  // versionOver = await versionCheck(0, 9, 11) //バージョンチェック
+  // versionOver0914 = await versionCheck(0, 9, 14) //バージョンチェック
+  if (logseq.settings?.booleanLinkedReferences === true)
+    provideStyle(keyNestingSide, CSS3Side,)
+  else
     logseq.provideStyle({ key: keyNonSide, style: CSSNonSide })
-  }
+
   if (logseq.settings?.booleanRightSidebar === true)
-    provideStyleRightSidebarEachVersion(
-      keyNestingRightSidebar,
-      keyNestingRightSidebar00914,
-      keyRightSidebar
-    )
+    provideStyleRightSidebar(keyRightSidebar,)
 
   //Fix bugs
   /* Fix "Extra space when journal queries are not active #6773" */
@@ -59,11 +45,11 @@ const main = async () => {
   logseq.provideStyle({ key: "main", style: CSSmain })
 
   logseq.onSettingsChanged((newSet: LSPluginBaseInfo["settings"], oldSet: LSPluginBaseInfo["settings"]) => {
-    if (newSet && oldSet && newSet !== oldSet) {
-      if (
-        oldSet.booleanLinkedReferences === true &&
-        newSet.booleanLinkedReferences === false
-      ) {
+    if (newSet
+      && oldSet
+      && newSet !== oldSet) {
+      if (oldSet.booleanLinkedReferences === true
+        && newSet.booleanLinkedReferences === false) {
         try {
           removeProvideStyle(keySide)
           removeProvideStyle(keyNestingSide)
@@ -71,38 +57,22 @@ const main = async () => {
           logseq.provideStyle({ key: keyNonSide, style: CSSNonSide })
         }
       } else
-        if (
-          oldSet.booleanLinkedReferences === false &&
-          newSet.booleanLinkedReferences === true
-        ) {
+        if (oldSet.booleanLinkedReferences === false
+          && newSet.booleanLinkedReferences === true) {
           try {
             removeProvideStyle(keyNonSide)
           } finally {
-            provideStyleByVersion(
-              versionOver,
-              keyNestingSide,
-              CSS3NestingSide,
-              keySide,
-              CSSside
-            )
+            provideStyle(keyNestingSide, CSS3Side,)
           }
         }
-      if (
-        oldSet.booleanRightSidebar === true &&
-        newSet.booleanRightSidebar === false
-      ) {
+      if (oldSet.booleanRightSidebar === true
+        && newSet.booleanRightSidebar === false) {
         removeProvideStyle(keyRightSidebar)
-        removeProvideStyle(keyNestingRightSidebar)
-        removeProvideStyle(keyNestingRightSidebar00914)
       } else
-        if (
-          oldSet.booleanRightSidebar === false &&
-          newSet.booleanRightSidebar === true
-        ) {
-          provideStyleRightSidebarEachVersion(
-            keyNestingRightSidebar,
-            keyNestingRightSidebar00914,
-            keyRightSidebar
+        if (oldSet.booleanRightSidebar === false
+          && newSet.booleanRightSidebar === true) {
+          provideStyleRightSidebar(
+            keyRightSidebar,
           )
         }
     }
@@ -111,3 +81,39 @@ const main = async () => {
 } /* end_main */
 
 logseq.ready(main).catch(console.error)
+
+
+// export async function versionCheck(
+//   first: number,
+//   second: number,
+//   third: number
+// ): Promise<boolean> {
+//   let version: string = await logseq.App.getInfo("version")
+//   //0.9.13-nightly.20230811のような文字列であった場合、-nightly以降を削除する
+//   if (version?.includes("-")) {
+//     const versionArr = version?.split("-") as string[]
+//     version = versionArr[0]
+//   }
+//   const versionArr = version?.split(".") as string[]
+//   if (
+//     Number(versionArr[0]) > first
+//     || (Number(versionArr[0]) === first
+//       && Number(versionArr[1]) > second)
+//     || (Number(versionArr[1]) === second
+//       && Number(versionArr[2]) >= third)
+//   )
+//     return true //指定した以上のバージョンの場合
+//   else
+//     return false //指定より古いバージョンの場合
+// }
+
+
+const provideStyle = (newKey: string, newStyle: string) => {
+  logseq.provideStyle({ key: newKey, style: newStyle })
+}
+
+
+const provideStyleRightSidebar = (keyRightSidebar: string) => {
+  logseq.provideStyle({ key: keyRightSidebar, style: CSS3RightSidebar, })
+}
+
--- src/lib.ts
+++ src/lib.ts
@@ -1,7 +1,3 @@
-import { versionOver, versionOver0914 } from "."
-import CSS3NestingRightSidebar from "./nestingRightSidebar.css?inline"
-import CSS3NestingRightSidebar00914 from "./nestingRightSidebar00914.css?inline"
-import CSSrightSidebar from "./rightSidebar.css?inline"
 
 export const removeProvideStyle = (className: string) => {
   const doc = parent.document.head.querySelector(`style[data-injected-style^="${className}"]`) as HTMLStyleElement | null
@@ -11,67 +7,4 @@ export const removeProvideStyle = (className: string) => {
 export const removeCSSclass = (className: string) => {
   if (parent.document.body.classList?.contains(className))
     parent.document.body.classList.remove(className)
-}
-
-
-export async function versionCheck(
-  first: number,
-  second: number,
-  third: number
-): Promise<boolean> {
-  let version: string = await logseq.App.getInfo("version")
-  //0.9.13-nightly.20230811のような文字列であった場合、-nightly以降を削除する
-  if (version?.includes("-")) {
-    const versionArr = version?.split("-") as string[]
-    version = versionArr[0]
-  }
-  const versionArr = version?.split(".") as string[]
-  if (
-    Number(versionArr[0]) > first ||
-    (Number(versionArr[0]) === first && Number(versionArr[1]) > second) ||
-    (Number(versionArr[1]) === second && Number(versionArr[2]) >= third)
-  )
-    return true //指定した以上のバージョンの場合
-  else
-    return false //指定より古いバージョンの場合
-}
-
-
-export function provideStyleByVersion(
-  versionOver: boolean,
-  newKey: string,
-  newStyle: string,
-  oldKey: string,
-  oldStyle: string
-) {
-  if (versionOver === true)
-    logseq.provideStyle({ key: newKey, style: newStyle })
-  //指定した以上のバージョンの場合
-  else
-    logseq.provideStyle({ key: oldKey, style: oldStyle }) //指定より古いバージョンの場合
-}
-
-
-export function provideStyleRightSidebarEachVersion(
-  keyNestingRightSidebar: string,
-  keyNestingRightSidebar00914: string,
-  keyRightSidebar: string
-) {
-  if (versionOver0914 === true) {
-    logseq.provideStyle({
-      key: keyNestingRightSidebar00914,
-      style: CSS3NestingRightSidebar00914,
-    })
-  } else
-    if (versionOver === true) {
-      logseq.provideStyle({
-        key: keyNestingRightSidebar,
-        style: CSS3NestingRightSidebar,
-      })
-    } else {
-      logseq.provideStyle({
-        key: keyRightSidebar,
-        style: CSSrightSidebar,
-      })
-    }
 }
